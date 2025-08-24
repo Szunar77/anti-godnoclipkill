ae299b4b81c38f8f3c2786083779c0a053099a52
Szunar <34723736+Szunar77@users.noreply.github.com>
2025-08-24 04:42:15 +0200

    Update antinocgodkill_server.lua
    oh my god

--- lua/autorun/server/antinocgodkill_server.lua
+++ lua/autorun/server/antinocgodkill_server.lua
@@ -3,7 +3,7 @@ print("Anti god-noclip kill script is running...")
 util.AddNetworkString("PlyTriedGNK")
 
 hook.Add("PlayerShouldTakeDamage", "PSTD", function(ply, ent)
-	if ent:IsValid() and not ent:InVehicle() then 
+	if ent:IsValid() and ent:IsPlayer() and not ent:InVehicle() then 
 		if ent:HasGodMode() or ent:GetMoveType() == 8 and ent ~= ply then 
 			print(ent:GetName() .. " [" .. ent:SteamID() .. "] tried attacking " .. ply:GetName() .. " [" .. ply:SteamID() .. "] with godmode/noclip on!" ) 
 			net.Start("PlyTriedGNK")
